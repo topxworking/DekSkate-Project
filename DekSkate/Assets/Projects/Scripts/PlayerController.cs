9235dc97e5a1ce8c3ab3c12e05023ea5591882a0
TOPX <topx.working@gmail.com>
2026-02-08 21:49:48 +0700

    add PlayerController.cs

---
+++ DekSkate/Assets/Projects/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float rotationSpeed = 15f;
+
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float jumpCutMultiplier = 0.5f;
+    [SerializeField] private float garvityScale = 2.5f;
+
+    [Header("Ground Detection")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody rb;
+    private Vector2 _moveInput;
+    private bool _isGrounded;
+    private PlayerControls _inputs;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        _inputs = new PlayerControls();
+
+        _inputs.Player.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
+        _inputs.Player.Move.canceled += ctx => _moveInput = Vector2.zero;
+
+        _inputs.Player.Jump.performed += ctx => Jump();
+        _inputs.Player.Jump.canceled += ctx => JumpCut();
+    }
+
+    private void OnEnable() => _inputs.Enable();
+    private void OnDisable() => _inputs.Disable();
+
+    private void Update()
+    {
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (_moveInput.x != 0)
+        {
+            float targetAngle = _moveInput.x > 0 ? 90f : -90f;
+            Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        ApplyGravity();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 targetVelocity = new Vector3(_moveInput.x * moveSpeed, rb.linearVelocity.y, 0);
+        rb.linearVelocity = targetVelocity;
+    }
+
+    private void ApplyGravity()
+    {
+        if (rb.useGravity)
+        {
+            rb.AddForce(Physics.gravity * (garvityScale - 1) * rb.mass);
+        }
+    }
+
+    private void Jump()
+    {
+        if (_isGrounded)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, 0);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void JumpCut()
+    {
+        if (rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier, 0);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+}
