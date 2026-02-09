19d37ac5f986966e4c7d179ba4f997f556e4009a
TOPX <topx.working@gmail.com>
2026-02-10 01:03:59 +0700

    add Object Pool and Update Game Scene

---
+++ DekSkate/Assets/Projects/Scripts/ObjectPoolingManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+using System.Collections.Generic;
+
+public class ObjectPoolingManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class PoolConfig
+    {
+        public string id;
+        public GameObject prefab;
+    }
+
+    public List<PoolConfig> configs;
+    private Dictionary<string, IObjectPool<GameObject>> _pools = new Dictionary<string, IObjectPool<GameObject>>();
+
+    void Awake()
+    {
+        foreach (var config in configs)
+        {
+            GameObject prefabToSpawn = config.prefab;
+            var pool = new ObjectPool<GameObject>(
+                createFunc: () => Instantiate(prefabToSpawn),
+                actionOnGet: obj => obj.SetActive(true),
+                actionOnRelease: obj => obj.SetActive(false)
+            );
+            _pools.Add(config.id, pool);
+        }
+    }
+
+    public GameObject GetFromPool(string id)
+    {
+        if (_pools.ContainsKey(id)) return _pools[id].Get();
+        Debug.LogError($"ไม่พบ Pool ที่มี ID: {id}");
+        return null;
+    }
+}
