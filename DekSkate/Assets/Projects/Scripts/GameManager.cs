48460dae781cb6a5509ab7e83173f49b7e1dda62
TOPX <topx.working@gmail.com>
2026-02-09 19:17:53 +0700

    remove PlayerHealth

--- DekSkate/Assets/Projects/Scripts/GameManager.cs
+++ DekSkate/Assets/Projects/Scripts/GameManager.cs
@@ -5,14 +5,12 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [Header("Player Stats")]
-    public float health = 100f;
     public int score = 0;
     public int starsCollected = 0;
     public float timeRemaining = 60f;
     public bool isGameActive = true;
 
     [Header("UI References")]
-    public Slider healthSlider;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI timeText;
     public GameObject winPanel;
@@ -38,7 +36,6 @@ public class GameManager : MonoBehaviour
 
     void UpdateUI()
     {
-        healthSlider.value = health;
         scoreText.text = "Score: " + score;
 
         int displayTime = Mathf.FloorToInt(timeRemaining);
