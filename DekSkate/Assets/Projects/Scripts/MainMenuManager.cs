0bfd70abf25b17e98a11d58eda1d6666673df2f8
TOPX <topx.working@gmail.com>
2026-02-20 02:19:51 +0700

    update main menu and add mute toggle

--- DekSkate/Assets/Projects/Scripts/MainMenuManager.cs
+++ DekSkate/Assets/Projects/Scripts/MainMenuManager.cs
@@ -5,6 +5,10 @@ public class MainMenuManager : MonoBehaviour
 {
     public PlayerData playerData;
 
+    public GameObject playButton;
+    public GameObject quitButton;
+    public GameObject settingsPanel;
+
     public void StartButton()
     {
         SceneManager.LoadScene("1-1");
@@ -15,5 +19,17 @@ public class MainMenuManager : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit Game");
+    }
+
+    public void SettingsButton()
+    {
+        if (settingsPanel != null)
+        {
+            bool isActive = settingsPanel.activeSelf;
+            settingsPanel.SetActive(!isActive);
+
+            playButton.SetActive(isActive);
+            quitButton.SetActive(isActive);
+        }
     }
 }
